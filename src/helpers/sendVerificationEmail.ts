0b7c53f2d916b35232f6a7ea8454a3bc292a59f0
Krishna <krishnarajput00009@gmail.com>
2024-04-23 23:52:51 +0530

    utlis

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,23 +1,28 @@
 import { resend } from "@/lib/resend/resend";
 import VerificationEmail from "../../emails/verificationEmail";
 import { ApiResponse} from "@/types/ApiResponse";
+import { NextResponse } from "next/server";
 
 
 export async function SendverificationEmail({
     email,username,otp
 }:{
     email:string,username:string,otp:string
-}):Promise<ApiResponse>{
+}){
     try {
-        await resend.emails.send({
+        const emailRes=await resend.emails.send({
             from: 'onboarding@resend.dev', 
             to: email,
             subject: 'Verification Email',
             react: VerificationEmail({username,otp}) ,
           });
-        return {success:true, message:"verification email send successfully.."}
+          console.log(emailRes)
+        return NextResponse.json<ApiResponse>({
+            success:true,
+            message:"verification email send successfully.."
+        },{status:200})
     } catch (error) {
         console.error("error in sending verification email",error)
-        return {success:false, message:"error in sending verification email"}
+        return NextResponse.json<ApiResponse>({success:false, message:"error in sending verification email",},{status:200})
     }
 }
