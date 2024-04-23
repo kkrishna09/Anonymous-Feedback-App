e6249ea66a9125128816413ee84181e4e8256511
Krishna <krishnarajput00009@gmail.com>
2024-04-23 23:15:15 +0530

    otpVerication

--- src/app/otpVerification/page.tsx
+++ src/app/otpVerification/page.tsx
@@ -7,12 +7,18 @@ import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp"
 import axios from 'axios'
 import { useToast } from '@/components/ui/use-toast'
 import { ApiResponse } from '@/types/ApiResponse'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
 
 export default function otpVerification() {
+    let username="";
     const {toast}=useToast()
     const queryString = window.location.search; 
-    const urlParams = new URLSearchParams(queryString); 
-    const username = urlParams.get('username')
+    if(queryString){
+        const urlParams = new URLSearchParams(queryString); 
+        username = urlParams.get('username') || ""
+    }
+    
     const [otpData,setOtpData]=useState({
         otp:"",
         username
@@ -34,26 +40,33 @@ export default function otpVerification() {
     }
   return (
 
-    <div>
-    <InputOTP onChange={(e)=> setOtpData(prev => ({...prev,otp: e}))}
-     maxLength={6} pattern={REGEXP_ONLY_DIGITS_AND_CHARS}>
-        <InputOTPGroup>
-            <InputOTPSlot index={0} />
-            <InputOTPSlot index={1} />
-            <InputOTPSlot index={2} />
-        </InputOTPGroup>
-        <InputOTPSeparator />
-        <InputOTPGroup>
-            <InputOTPSlot index={3} />
-            <InputOTPSlot index={4} />
-            <InputOTPSlot index={5} />
-        </InputOTPGroup>
-    </InputOTP>
+    <div   className='flex flex-col gap-5 px-20 pt-16 pb-7 bg-white border border-var(--border) shadow-md absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+        <h1 className='text-center  text-black  text-3xl font-bold'>Anonymous-Feedback-App</h1>
+        <h2 className="text-center  text-black  text-2xl font-bold">OTP Verification</h2>
+        <div><h2>Enter your 6 digit OTP</h2></div>
+    <div className='flex justify-center'> 
+    
+        <InputOTP  onChange={(e)=> setOtpData(prev => ({...prev,otp: e}))}
+            maxLength={6} pattern={REGEXP_ONLY_DIGITS_AND_CHARS}>
+            <InputOTPGroup>
+                <InputOTPSlot index={0} />
+                <InputOTPSlot index={1} />
+                <InputOTPSlot index={2} />
+            </InputOTPGroup>
+            <InputOTPSeparator />
+            <InputOTPGroup>
+                <InputOTPSlot index={3} />
+                <InputOTPSlot index={4} />
+                <InputOTPSlot index={5} />
+            </InputOTPGroup>
+        </InputOTP>
+    </div>
     <div>
     <Button onClick={handleSubmit} disabled={otpData.otp.length===6? false:true}>
         Submit
     </Button>
     </div>
+    <div className='mt-4'>Did't Sign Up yet? <Link className="text-blue-500" href={"http://localhost:3000/sign-in"}>Sign In</Link></div>
     </div>
   )
 }
