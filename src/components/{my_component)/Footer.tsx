5939da104a8f3768b7d0048a5d81a3d1f9946428
Krishna Kumar Singh <krishnarajput@0009@gmail.com>
2024-05-01 17:19:32 +0530

    navbar and footer

---
+++ src/components/{my_component)/Footer.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
+        © 2023 True Feedback. All rights reserved.
+      </footer>
+  )
+}
+
+export default Footer
